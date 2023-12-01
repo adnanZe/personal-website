022d671e8e5c42487dfafe1ec50d7337a396c567
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-01 20:27:14 +0200

    changed lato to poppins and added style to main

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Adnan Zecheria</h1>
+    <main className="flex justify-center items-center h-screen">
+      <h1 className="text-7xl font-black tracking-widest">Adnan Zecheria</h1>
     </main>
   );
 }
