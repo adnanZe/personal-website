8c6c7738c9206d45933e99d1d85c17c55765c084
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-03 19:16:46 +0200

    [feature] added transition for menu items

--- app/components/menuItems.tsx
+++ app/components/menuItems.tsx
@@ -18,31 +18,31 @@ export default function MenuItems() {
     <nav>
       <ol className="grid grid-cols-2 grid-rows-4 gap-4 absolute top-0 left-0 p-5 w-screen h-screen">
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 hover:bg-neutral-950 hover:bg-opacity-60"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("About me")}
           onMouseLeave={() => handleHoverOut()}>
           About me
         </li>
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 hover:bg-neutral-950 hover:bg-opacity-60"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("Experiences")}
           onMouseLeave={() => handleHoverOut()}>
           Experiences
         </li>
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 hover:bg-neutral-950 hover:bg-opacity-60"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("Projects")}
           onMouseLeave={() => handleHoverOut()}>
           Projects
         </li>
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer hover:bg-neutral-950 hover:bg-opacity-60"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("Contact")}
           onMouseLeave={() => handleHoverOut()}>
           Contact
         </li>
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer hover:bg-neutral-950 hover:bg-opacity-60"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("Settings")}
           onMouseLeave={() => handleHoverOut()}>
           Settings
