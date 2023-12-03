158aa8189b6d1db27d2937f8bc95c206d591bec5
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-03 21:33:08 +0200

    [wip] changed grid system

--- app/components/menuItems.tsx
+++ app/components/menuItems.tsx
@@ -16,33 +16,33 @@ export default function MenuItems() {
   }, [hoveredCtx]);
   return (
     <nav>
-      <ol className="grid grid-cols-2 grid-rows-4 gap-4 absolute top-0 left-0 p-5 w-screen h-screen">
+      <ol className="grid grid-cols-6 grid-rows-4 gap-4 absolute top-0 left-0 p-5 w-screen h-screen">
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 col-span-4 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("About me")}
           onMouseLeave={() => handleHoverOut()}>
           <span className="text-xl">About me</span>
         </li>
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 col-span-2 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("Experiences")}
           onMouseLeave={() => handleHoverOut()}>
           <span className="text-xl">Experiences</span>
         </li>
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer row-span-2 col-span-2 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("Projects")}
           onMouseLeave={() => handleHoverOut()}>
           <span className="text-xl">Projects</span>
         </li>
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer col-span-4 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("Contact")}
           onMouseLeave={() => handleHoverOut()}>
           <span className="text-xl">Contact</span>
         </li>
         <li
-          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
+          className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer col-span-4 hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500"
           onMouseEnter={() => handleHover("Settings")}
           onMouseLeave={() => handleHoverOut()}>
           <span className="text-xl">Settings</span>
