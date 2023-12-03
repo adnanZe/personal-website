987a722dc2ae0d7c571470d8f5661ae46294318a
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-03 14:15:50 +0200

    [wip] style navigation

--- app/components/gridNavigation.tsx
+++ app/components/gridNavigation.tsx
@@ -1,21 +1,19 @@
 export default function GridNavigation() {
+  const classNameGeneralStyle =
+    "bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur cursor-pointer";
+  const classNameHover = "hover:bg-neutral-950 hover:bg-opacity-60";
+  const classNameGridItemBig = "row-span-2";
+
+  const classNameConcatOne = `${classNameGeneralStyle} ${classNameHover} ${classNameGridItemBig}`;
+  const classNameConcatTwo = `${classNameGeneralStyle} ${classNameHover}`;
+
   return (
     <div className="grid grid-cols-2 grid-rows-4 gap-4 absolute top-0 left-0 p-5 w-screen h-screen">
-      <div className="bg-neutral-800 row-span-2 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur">
-        <span>About me</span>
-      </div>
-      <div className="bg-neutral-800 row-span-2 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur">
-        Experiences
-      </div>
-      <div className="bg-neutral-800 row-span-2 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur">
-        Projects
-      </div>
-      <div className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur">
-        Contact
-      </div>
-      <div className="bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-5 backdrop-blur">
-        Settings
-      </div>
+      <div className={classNameConcatOne}>About me</div>
+      <div className={classNameConcatOne}>Experiences</div>
+      <div className={classNameConcatOne}>Projects</div>
+      <div className={classNameConcatTwo}>Contact</div>
+      <div className={classNameConcatTwo}>Settings</div>
     </div>
   );
 }
