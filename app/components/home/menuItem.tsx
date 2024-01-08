be1f952d1e54a6a651aaff6f6e14524d42bf1b56
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-01-08 20:08:48 +0200

    [feature] added link href to menu items

--- app/components/home/menuItem.tsx
+++ app/components/home/menuItem.tsx
@@ -1,13 +1,19 @@
 import { useCallback, useContext } from "react";
 import AnimationContext from "../../store/animationContext";
 import AnimatedText from "../animatedText";
+import Link from "next/link";
 
 interface MenuItemProps {
   title: string;
   gridClassName: string;
+  href: string;
 }
 
-export default function MenuItem({ title, gridClassName }: MenuItemProps) {
+export default function MenuItem({
+  title,
+  gridClassName,
+  href,
+}: MenuItemProps) {
   const animationCtx = useContext(AnimationContext);
 
   const isTitleAnimationComplete = animationCtx.isTitleAnimationComplete;
@@ -33,18 +39,20 @@ export default function MenuItem({ title, gridClassName }: MenuItemProps) {
       onMouseEnter={handleHover}
       onMouseLeave={handleHoverOut}>
       <div className="h-full w-full animate-elevateContainer">
-        <div className="h-full w-full bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-2 sm:p-5 backdrop-blur hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500">
-          <span
-            className={`text-base absolute sm:relative sm:text-lg lg:text-xl ${
-              isTitleAnimationComplete ? "inline-block" : "hidden"
-            }`}>
-            {isMenuItemHovered || isMenuItemFinishedAnimation ? (
-              <AnimatedText name={title} isMenuItemHovered />
-            ) : (
-              title
-            )}
-          </span>
-        </div>
+        <Link href={`/${href}`} passHref>
+          <div className="h-full w-full bg-neutral-800 rounded-lg bg-opacity-60 flex items-end p-2 sm:p-5 backdrop-blur hover:bg-neutral-950 hover:bg-opacity-60 transition ease-in-out duration-500">
+            <span
+              className={`text-base absolute sm:relative sm:text-lg lg:text-xl ${
+                isTitleAnimationComplete ? "inline-block" : "hidden"
+              }`}>
+              {isMenuItemHovered || isMenuItemFinishedAnimation ? (
+                <AnimatedText name={title} isMenuItemHovered />
+              ) : (
+                title
+              )}
+            </span>
+          </div>
+        </Link>
       </div>
     </li>
   );
--- app/components/home/menuItems.tsx
+++ app/components/home/menuItems.tsx
@@ -9,14 +9,31 @@ export default function MenuItems() {
     isVisibleMenu && (
       <nav>
         <ol className="grid grid-cols-1 sm:grid-cols-6 grid-rows-8 sm:grid-rows-4 gap-5 absolute top-0 left-0 p-5 w-screen h-screen">
-          <MenuItem title="About me" gridClassName="sm:col-span-4 row-span-2" />
+          <MenuItem
+            title="About me"
+            gridClassName="sm:col-span-4 row-span-2"
+            href="about"
+          />
           <MenuItem
             title="Experiences"
             gridClassName="sm:col-span-2 row-span-2"
+            href="experiences"
           />
-          <MenuItem title="Projects" gridClassName="sm:col-span-3 row-span-2" />
-          <MenuItem title="Contact" gridClassName="sm:col-span-3 row-span-1" />
-          <MenuItem title="Settings" gridClassName="sm:col-span-3 row-span-1" />
+          <MenuItem
+            title="Projects"
+            gridClassName="sm:col-span-3 row-span-2"
+            href="projects"
+          />
+          <MenuItem
+            title="Contact"
+            gridClassName="sm:col-span-3 row-span-1"
+            href="contact"
+          />
+          <MenuItem
+            title="Settings"
+            gridClassName="sm:col-span-3 row-span-1"
+            href="settings"
+          />
         </ol>
       </nav>
     )
