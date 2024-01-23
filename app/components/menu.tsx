8f3a6e9e85041a3f76f2e69b27dbc206748f74f4
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-01-23 22:39:36 +0200

    [wip] menu

---
+++ app/components/menu.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>About me</li>
+        <li>Experiences</li>
+        <li>Projects</li>
+        <li>Contact</li>
+        <li>Settings</li>
+      </ul>
+    </nav>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./_styles/globals.css";
 import { MAIN_FONT } from "./_constants/fonts";
 import { FIRST_NAME, LAST_NAME } from "./_constants/name";
+import Menu from "./components/menu";
+import React from "react";
 
 export const metadata: Metadata = {
   title: `${FIRST_NAME} ${LAST_NAME} | Frontend Developer`,
@@ -13,9 +15,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isRoot =
+    React.isValidElement(children) &&
+    children.props.childPropSegment === "__PAGE__";
+
   return (
     <html lang="en">
-      <body className={MAIN_FONT.className}>{children}</body>
+      <body className={MAIN_FONT.className}>
+        {children}
+        {!isRoot && <Menu />}
+      </body>
     </html>
   );
 }
