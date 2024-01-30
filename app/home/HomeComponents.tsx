708635076f5dd8ba4356f5828cee9fa1bb969bca
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-01-30 19:05:44 +0200

    [wip] refactoring

---
+++ app/home/HomeComponents.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainTitle from "./mainTitle";
+import MenuItems from "./menuItems";
+
+export default function HomeComponents() {
+  return (
+    <>
+      <MainTitle />
+      <MenuItems />
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { AnimationContextProvider } from "./_store/animationContext";
-import MainTitle from "./home/mainTitle";
-import MenuItems from "./home/menuItems";
+import HomeComponents from "./home/HomeComponents";
 
 export default function Home() {
   return (
     <AnimationContextProvider>
       <main className="flex justify-center items-center h-screen">
-        <MainTitle />
-        <MenuItems />
+        <HomeComponents />
       </main>
     </AnimationContextProvider>
   );
